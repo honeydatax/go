964ea6a30934e1b3b9a867c3949ca93ac4d8b8bb
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-09-09 16:52:35 +0100

    Add files via upload

---
+++ sends.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func bar(ss string ,ii int) string{
+	var s string=""
+	var i int = 0
+	for i=0;i<ii;i++{
+		s=s+ss;
+	}
+	return s
+}
+
+func barsize(ss string ,sss string,ii int) string{
+	var s string=""
+	var i int = 0
+	var sz int = 0
+	s=ss
+	sz=ii-len(ss)
+	for i=0;i<sz;i++{
+		s=s+sss;
+	}
+	return s
+}
+
+
+func center(s string,size int)string{
+	var i int =0
+	var s1 string=""
+	i=(size/2)-(len(s)/2)
+	s1=bar(" ",i)
+	s1=s1+s
+	return s1
+}
+
+func barrigth(ss string ,sss string,ii int) string{
+	var s string=""
+	var i int = 0
+	var sz int = 0
+	s=ss
+	sz=ii-len(ss)
+	for i=0;i<sz;i++{
+		s=sss+s;
+	}
+	return s
+}
+
+
+func main(){
+	var a int = 0
+	for  a = 0 ; a < 16 ; a++  {
+
+			fmt.Printf("%s\n",barrigth(bar("*",a+1)," ",32))
+
+	}
+}
